7f34f314d0efa1fabaf0b150977950c5aa199d95
debasish <debasish303@gmail.com>
2018-08-13 11:34:36 +0530

    Initial file for 00.

---
+++ play.php
@@ -0,0 +1,51 @@
+<?php
+class Ship {
+    public $name;
+    public $weaponPower = 0;
+    public $jediFactor = 0;
+    public $strength = 0;
+    public function sayHello() {
+        echo "<strong>Battle ships</strong>";
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getNameAndSpecs($useShortFormat) {
+        if ($useShortFormat) {
+            return sprintf(
+                '%s: %s /%s /%s',
+                $this->name, 
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+
+        } else {
+            return sprintf(
+                '%s: w:%s , j:%s , s:%s',
+                $this->name,
+                $this->weaponPower,
+                $this->jediFactor,
+                $this->strength
+            );
+        }
+    }
+}
+
+function printShipSummary($someShip) {
+    echo 'Ship name: '.$someShip->name;
+    echo "<hr/>";
+    echo $someShip->sayHello();
+    echo "<hr/>";
+    echo $someShip->getName();
+    echo "<hr/>";
+    echo $someShip->getNameAndSpecs(false);
+    echo "<hr/>";
+    echo $someShip->getNameAndSpecs(true);
+}
+$myShip = new Ship();
+$myShip->name = 'Jedi Starship';
+$myShip->weaponPower = 10;
+printShipSummary($myShip);
+
